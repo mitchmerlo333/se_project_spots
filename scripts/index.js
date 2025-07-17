1a81eb491f1fae83ad4565db290cdfd3049bf46b
Mitchell Merlo <mitchellfm96@aol.com>
2025-07-17 09:45:47 -0400

    4.5.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,20 @@
 const editProfileBtn = document.querySelector(".profile__edt-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 const editProfileForm = editProfileModal.querySelector(".modal__form");
-const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
-
-
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-
-const profileNameEl = document.querySelector("click", function () {}
+const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
-
-
 
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
@@ -35,11 +34,9 @@ newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal-is-open");
 });
 
-function handleEditProfileSubmit(evt) {
-  evt.preventDefault();
-  profileNameEl.textContent = editProfileNameInput.value
+editProfileForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
   editProfileModal.classList.remove("modal-is-open");
-}
-
-editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+});
